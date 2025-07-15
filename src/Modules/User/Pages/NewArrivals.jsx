ce80cc91406e0fe1d3b0a67d8446b2d64d86a2a6
SanathRai <sanathrai03@gmail.com>
2025-07-16 00:58:49 +0530

    Changed style of OfferCard and NewArrivalCard also used days package

--- src/Modules/User/Pages/NewArrivals.jsx
+++ src/Modules/User/Pages/NewArrivals.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { CircularProgress,  } from '@mui/material';
+import { CircularProgress, } from '@mui/material';
 import '../CSS/Home.css';
-import { Container, Grid, Card, CardMedia, CardContent, Typography, CardActions, Button, Box } from "@mui/material";
+import { Container, Grid, Card, CardMedia, CardContent, Typography, CardActions, Box } from "@mui/material";
 import axios from "axios";
 
 const NewArrivals = () => {
@@ -34,17 +34,17 @@ const NewArrivals = () => {
             <Grid container spacing={4}>
 
                 {loading ?
-                    (<Box sx={{ minWidth: "100%", minHeight:"100px", display: 'flex', alignItems: 'center', justifyContent: "center"}}> <CircularProgress /> </Box>) :
+                    (<Box sx={{ minWidth: "100%", minHeight: "100px", display: 'flex', alignItems: 'center', justifyContent: "center" }}> <CircularProgress /> </Box>) :
 
                     error ?
-                        (<Box sx={{ minWidth: "100%", minHeight:"100px", display: 'flex', alignItems: 'center', justifyContent: "center", backgroundColor: 'yellow'}}> <Typography variant="body1" sx={{ m: 2 }}> Failed to get New Arrivals. </Typography> </Box>) :
+                        (<Box sx={{ minWidth: "100%", minHeight: "100px", display: 'flex', alignItems: 'center', justifyContent: "center", backgroundColor: 'yellow' }}> <Typography variant="body1" sx={{ m: 2 }}> Failed to get New Arrivals. </Typography> </Box>) :
 
                         newProducts.length === 0 ? (
-                            <Box sx={{ minWidth: "100%", minHeight:"100px", display: 'flex', alignItems: 'center', justifyContent: "center"}}><Typography variant="h6">No new arrivals in the last 7 days.</Typography></Box>
+                            <Box sx={{ minWidth: "100%", minHeight: "100px", display: 'flex', alignItems: 'center', justifyContent: "center" }}><Typography variant="h6">No new arrivals in the last 7 days.</Typography></Box>
                         ) : (
                             <Grid item xs={12} sm={6} md={4} px={6} sx={{ display: 'flex', alignItems: 'center', flexWrap: "nowrap", gap: 5, minWidth: "100%", overflowX: "scroll", scrollbarWidth: 'none', '&::-webkit-scrollbar': { display: 'none', }, }}>
                                 {newProducts.map((product) => (
-                                    <Card key={product.id} sx={{ minWidth: 350, maxWidth: 350, borderRadius: '16px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
+                                    <Card key={product.id} sx={{ minWidth: 350, maxWidth: 350, borderRadius: '16px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', cursor: 'pointer' }} onClick={() => navigate(`/product/${product._id}`)}>
                                         <CardMedia component="img" height="200" image={product.images[0]} alt={product.name} sx={{ objectFit: "contain" }} />
                                         <CardContent>
                                             <Typography variant="h6">{product.name}</Typography>
@@ -56,9 +56,6 @@ const NewArrivals = () => {
                                             </Typography>
                                         </CardContent>
                                         <CardActions>
-                                            <Button variant="contained" color="primary" fullWidth onClick={() => navigate(`/product/${product._id}`)} >
-                                                View Product
-                                            </Button>
                                         </CardActions>
                                     </Card>))}
                             </Grid>
--- src/Modules/User/Pages/Offers.jsx
+++ src/Modules/User/Pages/Offers.jsx
@@ -2,17 +2,28 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { CircularProgress } from '@mui/material';
 import '../CSS/Home.css';
-import { Container, Typography, Grid, Card, CardMedia, CardContent, Chip, Box, Button, } from "@mui/material";
+// import { useCart } from "../../../Context/CartContext"; 
+import { Container, Typography, Grid, Card, CardMedia, CardContent, Chip, Box, } from "@mui/material";
 import axios from "axios";
-
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 const Offer = () => {
 
   const [activeOffers, setActiveOffers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  // const { addToCart } = useCart();
+
+  dayjs.extend(relativeTime);
 
   const navigate = useNavigate();
+
+  // const handleCart = async (e) => {
+  //       e.stopPropagation();
+  //       addToCart(product)
+  //       console.log(e)
+  //   }
 
   useEffect(() => {
     const fetchNewArrivals = async () => {
@@ -35,12 +46,12 @@ const Offer = () => {
       </Typography>
       <Grid container spacing={3} >
         {loading ?
-          (<Box sx={{ minWidth: "100%", minHeight:"100px", display: 'flex', alignItems: 'center', justifyContent: "center"}}>
+          (<Box sx={{ minWidth: "100%", minHeight: "100px", display: 'flex', alignItems: 'center', justifyContent: "center" }}>
             <CircularProgress />
           </Box>) :
 
           error ?
-            (<Box sx={{ minWidth: "100%", minHeight:"100px", display: 'flex', alignItems: 'center', justifyContent: "center"}}>
+            (<Box sx={{ minWidth: "100%", minHeight: "100px", display: 'flex', alignItems: 'center', justifyContent: "center" }}>
               <Typography variant="body1" sx={{ m: 2 }}>
                 Failed to get Offers.
               </Typography>
@@ -49,28 +60,28 @@ const Offer = () => {
             (activeOffers.length > 0 ? (
               <Grid item xs={12} sm={6} md={4} px={6} sx={{ minWidth: "100%", minHeight: "440px", display: 'flex', alignItems: 'center', flexWrap: "nowrap", gap: 3, overflowX: "auto", scrollbarWidth: 'none', '&::-webkit-scrollbar': { display: 'none' } }} >
                 {activeOffers.map((product) => (
-                  <Card key={product.id} sx={{ borderRadius: '16px', minWidth: '300px', maxWidth: '350px' }}>
+                  <Card key={product.id} sx={{ borderRadius: '16px', minWidth: '300px', maxWidth: '350px', cursor: 'pointer' }} onClick={() => navigate(`/product/${product._id}`)}>
                     <CardMedia component="img" height="200" image={product.images[0]} alt={product.name} sx={{ objectFit: "contain" }} />
                     <CardContent>
                       <Typography variant="h6" height={32} overflow={'hidden'} >{product.name}</Typography>
-                      <Typography variant="body2" gutterBottom>
+                      <Typography variant="body2" gutterBottom maxHeight={40} overflow={'hidden'}>
                         {product.description}
                       </Typography>
                       <Typography color="primary">₹{product.price}</Typography>
-                      <Box sx={{ mt: 1, display: "flex", gap: 1, flexWrap: "wrap" }}>
+                      <Box sx={{ mt: 1, display: "flex", justifyContent: "space-between", flexWrap: "wrap" }}>
                         <Chip label={`${product.discount}% OFF`} color="primary" />
-                        {/* <Timer endTime={product.offerExpiresAt} /> */}
+                        <Chip label={`Offer ends ${dayjs(product.offerExpiresAt).fromNow()}`} color="secondary" />
                       </Box>
-                      <Button variant="contained" color="secondary" fullWidth sx={{ mt: 2 }} onClick={() => navigate(`/product/${product._id}`)} > Add to Cart </Button>
+                      {/* <Button variant="contained" color="secondary" fullWidth sx={{ mt: 2 }} onClick={handleCart} disabled={1 <= 0} > Add to Cart </Button> */}
                     </CardContent>
                   </Card>
                 ))}
               </Grid>
             ) : (
-              <Box sx={{ minWidth: "100%", minHeight:"100px", display: 'flex', alignItems: 'center', justifyContent: "center"}}>
-              <Typography variant="body1" sx={{ m: 2 }}>
-                No active offers available.
-              </Typography>
+              <Box sx={{ minWidth: "100%", minHeight: "100px", display: 'flex', alignItems: 'center', justifyContent: "center" }}>
+                <Typography variant="body1" sx={{ m: 2 }}>
+                  No active offers available.
+                </Typography>
               </Box>
             ))}
       </Grid>
